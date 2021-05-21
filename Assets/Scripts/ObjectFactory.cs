5942e4954497c82ec34f7f5540a3c07ba7558af9
Vivian Azalia <55457999+vivianazalia@users.noreply.github.com>
2021-05-21 12:32:08 +0700

    Add script pacman, ISpawn, Food, GameManager, Tile

---
+++ Assets/Scripts/ObjectFactory.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//class factory pattern
+public class ObjectFactory : MonoBehaviour
+{
+    #region singleton
+    private static ObjectFactory instance = null;
+    
+    public static ObjectFactory Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectOfType<ObjectFactory>();
+            }
+
+            return instance;
+        }
+    }
+    #endregion
+
+    [SerializeField] private Fruit fruitPrefab;
+    //[SerializeField] private Food foodPrefab;
+
+    public List<GameObject> objects;
+
+    private void Start()
+    {
+        objects = new List<GameObject>();
+        AddObjectToList();
+    }
+
+    //fungsi yang dipanggil ketika akan men-create object melalui factory 
+    public ISpawn GetObject(string name)
+    {
+        if (name == "Fruit")
+        {
+            //cari prefab food dari list objects
+            foreach (GameObject obj in objects)
+            {
+                if (obj.name == name)
+                {
+                    //clone prefab
+                    Fruit fruitObj = Instantiate(obj).GetComponent<Fruit>();
+                    fruitObj.SpawnPosition();
+                    StartCoroutine(DestroyFruit(fruitObj));
+
+                    //return prefab
+                    return fruitObj;
+                }
+            }
+        } else if(name == "Food")
+        {
+            foreach(GameObject obj in objects)
+            {
+                if (obj.name == name)
+                {
+                    //Food foodObj = Instantiate(obj).GetComponent<Food>();
+                    //return foodObj;
+                }
+            }
+        }
+        //tambahkan else if disini untuk create object dengan tipe ISpawn lainnya 
+
+        return null;
+    }
+
+    //untuk memasukkan prefab object ke list objects
+    void AddObjectToList()
+    {
+        objects.Add(fruitPrefab.gameObject);
+        //objects.Add(foodPrefab.gameObject);
+    }
+
+    //untuk menghapus atau men-destroy object fruit setelah spawn selama 3 detik.
+    //mungkin fungsi ini akan dipindahkan ke class Board
+    IEnumerator DestroyFruit(Fruit fruitObj)
+    {
+        if(fruitObj != null)
+        {
+            yield return new WaitForSeconds(5);
+            fruitObj.DestroyFruit();
+        }
+    }
+}
---
+++ Assets/Scripts/Pacman.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacman : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private Vector2 movement;
+    private Rigidbody2D rbPacman;
+
+    [SerializeField] private Tile startTile;
+
+    private int score;
+
+    public Tile TargetPosition { get; private set; }
+
+    private void Start()
+    {
+        rbPacman = GetComponent<Rigidbody2D>();
+        transform.position = startTile.GetComponent<Transform>().position;
+        ResetMovementValue();
+    }
+
+    private void Update()
+    {
+        CheckInput();
+    }
+    private void FixedUpdate()
+    {
+        Run();
+    }
+
+    //fungsi untuk mengatur movement Pacman
+    void Run() 
+    {
+        rbPacman.MovePosition(rbPacman.position + movement * speed * Time.fixedDeltaTime);
+        //rbPacman.MovePosition(TargetPosition.transform.position);
+    }
+
+    //fungsi untuk mengatur input movement Pacman
+    void CheckInput() 
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SetMovementValue(0, 1);
+        } 
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            SetMovementValue(1, 0);
+        } 
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            SetMovementValue(0, -1);
+        } 
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            SetMovementValue(-1, 0);
+        }
+    }
+
+    //untuk konfigurasi variable vector2 movement
+    public void SetMovementValue(float x, float y)
+    {
+        movement.x = x;
+        movement.y = y;
+    }
+
+    public void SetTargetPosition(Tile targetPos)
+    {
+        TargetPosition = targetPos;
+    }
+
+    //untuk reset arah movement pada awal level
+    public void ResetMovementValue()
+    {
+        SetMovementValue(1, 0);
+    }
+
+    //setter variable score
+    public void SetScore(int s)
+    {
+        score += s;
+    }
+
+    //getter variable score
+    public int GetScore()
+    {
+        return score;
+    }
+}
