85bea8262f7a25b155b46e1aab227c377632b348
Vivian Azalia <55457999+vivianazalia@users.noreply.github.com>
2021-05-24 17:48:44 +0700

    Pacman Eat Pellet

---
+++ Assets/Scripts/Pellet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pellet : MonoBehaviour
+{
+    public bool isEnergizerPellet;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
